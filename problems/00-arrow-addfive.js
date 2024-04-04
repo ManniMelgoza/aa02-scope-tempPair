65bf2c8fd021d3f03a3ebb14ed174eb32eee9902
Manni <emmanuelmelgoza1@gmail.com>
2024-04-03 22:25:58 -0600

    Solved 00 D:MM / N:EC

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -16,6 +16,13 @@ let result3 = addFive(37); // returns 42
 ***********************************************************************/
 
 // Your code here
+const addFive = (number) => number + 5;
+
+// let result1 = addFive(0); // returns 5
+let result2 = addFive(10); // returns 15
+// let result3 = addFive(37); // returns 42
+
+console.log(result2);
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
